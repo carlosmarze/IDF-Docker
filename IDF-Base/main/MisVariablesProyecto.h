dfa14709a7e7adb8bac09c8fb2dae9b9e1abfade
carlosmarze <c_arze@hotmail.com>
2026-06-17 18:54:38 -0300

    cambiamos el default sensorID a 0001

--- IDF-Base/main/MisVariablesProyecto.h
+++ IDF-Base/main/MisVariablesProyecto.h
@@ -1,10 +1,10 @@
 #ifndef MISVARIABLESPROYECTO_H
     #define MISVARIABLESPROYECTO_H
-    #define version_info "IDF6BAS20260617_V7" // Definición de la versión del firmware
+    #define version_info "IDF6BAS20260617_V8" // Definición de la versión del firmware
     //extern int SensorID ; // Identificador único del sensor lo definiremos en main.cpp o donde le pongamos el valor (leido de file seguramente)
     //#define ESQUEMA "ESP32IDF" // Esquema de datos para miTS, lo definimos como constante porque no cambia, pero podría ser una variable si se quisiera usar el mismo firmware para distintos esquemas.
     #define WRITE_API_KEY "WR4QL85BR9KIBP6V" // Write API Key para miTS, lo definimos como constante porque no cambia, pero podría ser una variable si se quisiera usar el mismo firmware para distintos sensores.
     #define READ_API_KEY "REZAQ4BH81OQP9PZ" // Read API Key para miTS, lo definimos como constante porque no cambia, pero podría ser una variable si se quisiera usar el mismo firmware para distintos sensores.
     
-    #define SENSORID 7001 // SensorID DEFAULT
+    #define SENSORID 0001 // SensorID DEFAULT
 #endif
